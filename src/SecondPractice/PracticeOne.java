9288ae1fb7185b9f737efcb78c39ddeaff30c178
Fardeen Qazizada <ahmad_fardeen@hotmail.com>
2023-05-17 12:54:13 -0400

    First class added

---
+++ src/SecondPractice/PracticeOne.java
@@ -0,0 +1,11 @@
+package SecondPractice;
+
+public class PracticeOne {
+
+	public static void main(String[] args) {
+		System.out.println("First Practice");
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Admin
+ *
+ */
+module fardeen_git_javaone {
+}
